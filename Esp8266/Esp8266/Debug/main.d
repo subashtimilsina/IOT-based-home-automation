99e0b6b6cc52a1d84d16fb92b7239328e502c4f8
Subash Timilsina <072bex442.subash@pcampus.edu.np>
2018-07-23 01:08:39 +0545

    Improvised version

--- Esp8266/Esp8266/Debug/main.d
+++ Esp8266/Esp8266/Debug/main.d
@@ -14,12 +14,12 @@ main.d main.o: .././main.cpp \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h \
- c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdlib.h \
- c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stddef.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdio.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdarg.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stddef.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\string.h \
- .././uart.h
+ .././uart.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdlib.h
 
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h:
 
@@ -51,14 +51,14 @@ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\
 
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h:
 
-c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdlib.h:
-
-c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stddef.h:
-
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdio.h:
 
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdarg.h:
 
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stddef.h:
+
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\string.h:
 
 .././uart.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdlib.h:
--- Esp8266/Esp8266/main.cpp
+++ Esp8266/Esp8266/main.cpp
@@ -12,118 +12,204 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <util/delay.h>
-#include <stdlib.h>
 #include <stdio.h>
-#include <string.h>
+#include "string.h"
 #include "uart.h"
 
-volatile int count;
+#define  DEBUG_BOOL				true
+#define  BUFFER_SIZE_RESPONSE	30
+
+#ifndef F_CPU
+#define F_CPU 16000000UL
+#endif
+
+#define clockCyclesPerMicrosecond() ( F_CPU / 1000000L )
+#define clockCyclesToMicroseconds (a) ( ((a) * 1000L) / (F_CPU / 1000L) )
+#define MICROSECONDS_PER_TIMER2_OVERFLOW (clockCyclesToMicroseconds(64 * 256))
+#define MILLIS_INC (MICROSECONDS_PER_TIMER2_OVERFLOW / 1000)
+#define FRACT_INC ((MICROSECONDS_PER_TIMER2_OVERFLOW % 1000) >> 3)
+#define FRACT_MAX (1000 >> 3)
+
+
+		
+unsigned long _timeout = 1000;		//1 second time out
+unsigned long _startMillis;
+
 char response[100];
-char c;
 
-void start_timer()
+
+volatile unsigned long timer2_millis = 0;
+volatile unsigned long timer2_fract = 0;
+
+
+unsigned long millis()
 {
-	TCCR0A |= (1<<WGM01);
-	TCCR0B |= (1<<CS00)|(1<<CS02);
-	TIMSK0 |= (1<<OCIE0A);
-	OCR0A = 156;		//1 centi seconds 	
-	TCNT0 = 0;
+	unsigned long m;
+	uint8_t oldSREG = SREG;
+	cli();
+	m = timer2_millis;
+	SREG = oldSREG;
+	
+	return m;
 }
 
-void stop_timer()
+
+void initialise_timeperiod()
 {
-	TCCR0A=0;
-	TCCR0B =0;
-	TIMSK0 &= ~(1<<OCIE0A);
-	count = 0;
+	TCCR2B |= (1<<CS22);
+	TIMSK2 |= (1<<TOIE2);
+	TCNT2 = 0;
 }
 
-const char* sendData(char *command,const int timeout) //timeout in centi seconds
+struct MultiTarget {
+	const char *str;
+	size_t len;
+	size_t index;
+};
+
+int timedRead()
+{
+	int c;
+	_startMillis = millis();
+	do {
+		c = uart0_getc();
+		if (c >= 0) return c;
+	} while(millis() - _startMillis < _timeout);
+	return -1;     
+}
+
+int findMulti( struct MultiTarget *targets, int tCount) {
+	for (struct MultiTarget *t = targets; t < targets+tCount; ++t) {
+		if (t->len <= 0)
+		return t - targets;
+	}
+
+	while (1) {
+		int c = timedRead();
+		if (c < 0)
+		return -1;
+
+		for (struct MultiTarget *t = targets; t < targets+tCount; ++t) {
+			if (c == t->str[t->index]) {
+				if (++t->index == t->len)
+				return t - targets;
+				else
+				continue;
+			}
+
+			if (t->index == 0)
+			continue;
+
+			int origIndex = t->index;
+			do {
+				--t->index;
+				if (c != t->str[t->index])
+				continue;
+
+				if (t->index == 0) {
+					t->index++;
+					break;
+				}
+				int diff = origIndex - t->index;
+				size_t i;
+				for (i = 0; i < t->index; ++i) {
+					if (t->str[i] != t->str[i + diff])
+					break;
+				}
+				if (i == t->index) {
+					t->index++;
+					break;
+				}
+			} while (t->index);
+		}
+	}
+	return -1;
+}
+
+
+bool findUntil(char *target, size_t targetLen)
+{
+		MultiTarget t[1] = {{target, targetLen, 0}};
+		return findMulti(t, 1) == 0 ? true : false;
+}
+
+bool  find(char *target)
+{
+	return findUntil(target, strlen(target));
+}
+
+char *sendData(const char* command, const uint16_t timeout, bool debug)
 {
 	memset(response,'\0',100);
 	uart0_puts(command);
-	start_timer();
-	while(timeout >= count)
+    long unsigned time = millis();
+	while( (time+timeout) > millis())
 	{
 		while(uart0_available())
 		{
-			c = uart0_getc();
-			strcat(response,&c);
+			char c[2];
+			c[1] = uart0_getc();
+			c[2] = '\0';
+			strcat(response,c);
 		}
 	}
-	stop_timer();
+	if(debug)
+	{
+		uart0_puts(response);         //to check whether the esp is sending data or not
+	}
 	return response;
 }
 
-
-void initialise()
+void initialize_esp8266()
 {
-	uart0_init(UART_BAUD_SELECT(115200,F_CPU));
-	sei();
-	DDRB |= 0X38; //set pb3 pb4 and pb5 as a output pin
-	count = 0;
-	sendData("AT+RST\r\n",200);
-	sendData("AT+CWJAP=\"Subash's Wifi\",\"bastain123\"\r\n",700);
-	sendData("AT+CWMODE=3\r\n",200);
-	sendData("AT+CIFSR\r\n",200);
-	sendData("AT+CIPMUX=1\r\n",200);
-	sendData("AT+CIPSERVER=1,80\r\n",200);
+	//set pb3,pb4 and pb5 as output
+	  DDRB |= 0X38;
+	  sendData("AT+RST\r\n",2000,DEBUG_BOOL);
+	  sendData("AT+CWJAP=\"Subash's Wifi\",\"Bastain123\"\r\n",7000,DEBUG_BOOL);
+	  sendData("AT+CWMODE=3\r\n",2000,DEBUG_BOOL);
+	  sendData("AT+CIFSR\r\n",2000,DEBUG_BOOL);
+	  sendData("AT+CIPMUX=1\r\n",2000,DEBUG_BOOL);
+	  sendData("AT+CIPSERVER=1,80\r\n",2000,DEBUG_BOOL);
 }
 
-
-bool find(char *substring)
-{
-	uint8_t i;
-	uint8_t length = strlen(substring);
-	memset(response,'\0',100);
-	bool search = false;
-	while(uart0_available())
-	{
-		c = uart0_getc();
-		strcat(response,&c);
-	}
-	if (response[0] != '\0')
-	{
-		for(i=0;i<strlen(response);i++)
-		{
-			for(uint8_t j=0;j<length;j++)
-			{
-				if (substring[j] == response[i])
-					continue;
-				else 
-					break;		
-				search = true;
-			}
-		}
-	}
-	return search;
-}
 
 int main()
 {
-	initialise();	
+	initialise_timeperiod();
+	uart0_init(UART_BAUD_SELECT(9600,F_CPU));
+	sei();
+	initialize_esp8266();
 	while(1)
 	{
-		if (find("+IPD"))
-		{
-			_delay_ms(1000);
-			int connectionId = uart0_getc()-48;
-			find("pin=");
-			int pinnumber = (uart0_getc()-48)*10;
-			pinnumber += (uart0_getc()-48);
-			if (pinnumber == 13)
-				PORTB = 0X20;
-			else if(pinnumber == 12)
-				PORTB = 0X10;
-			char closecommand[30];
-			sprintf(closecommand,"AT+CIPCLOSE=%d\r\n",connectionId);
-			sendData(closecommand,100);	
-		}		
+		  if(uart0_available())
+		  {
+			  if(find("+IPD,"))
+			  {
+				  _delay_ms(1000);
+				  int connectionId = uart0_getc()-48;
+				  find("pin=");
+				  int pinNumber = (uart0_getc() - 48)*10;
+				  pinNumber += (uart0_getc()-48);
+				  
+				  if(pinNumber == 13)
+					PORTB = 0X20;
+				 else if(pinNumber == 12)
+					PORTB = 0X10;
+				    
+				  char closeCommand[30];
+				  sprintf(closeCommand,"AT+CIPCLOSE=%d\r\n",connectionId);
+				  sendData(closeCommand,1000,DEBUG_BOOL);
+			  }
+		  }
 	}
-
+	return 0;
 }
 
-ISR(TIMER0_COMPA_vect)
-{
-	count++;
+ISR(TIMER2_OVF_vect) {
+	timer2_millis += 1;
+	timer2_fract += 3;
+	if (timer2_fract >= 125) {
+		timer2_fract -= 125;
+		timer2_millis += 1;
+	}
 }
